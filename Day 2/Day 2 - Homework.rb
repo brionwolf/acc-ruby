83138d0f895dcab793ecbe2a287326b01876d4d0
Max Escher <brionwolf@users.noreply.github.com>
2015-08-14 09:11:48 -0400

    Renamed files, added new ones in progress
    Renamed files for better organization, also started a new homework file
    for the week. Still a work in progress of course.

---
+++ Day 2/Day 2 - Homework.rb
@@ -0,0 +1,26 @@
+# Ruby Homework 1 due Monday August 17, 2015
+
+# --------------------------------------------------------------------------------------
+# HOME WORK
+# Create a few programs using arrays and hashes.
+# But make sure to use methods we have no discussed (look up the ruby documentation).
+# Create and add a method to the program.
+# --------------------------------------------------------------------------------------
+
+# --------------------------------------------------------------------------------------
+# Ideas
+# --------------------------------------------------------------------------------------
+
+=begin
+######
+
+1. Command line based game involving many if else statements
+	- How can it involve a method?
+
+2. A ruby based notes application.
+
+3. A program that calculates how long until your birthday
+
+
+=end
+######
